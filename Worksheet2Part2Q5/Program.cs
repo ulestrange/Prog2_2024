9c8149c1f86f4661545700b3aeef6ee9e4e364ee
Una L <una.lestrange@atu.ie>
2024-02-07 22:40:13 +0000

    Finsihed Q4 part 2

---
+++ Worksheet2Part2Q5/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Worksheet2Part2Q5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfStores = 5;
+
+            int[] storesSales = new int[numberOfStores];
+
+
+            for (int i =0; i < numberOfStores; i++)
+            {
+                Console.Write($"Enter sales for store {i+1}:");
+                storesSales[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i =0; i < numberOfStores; i++)
+            {
+                Console.Write($"Store{i+1}");
+
+                for (int j=0;j < storesSales[i] /100; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Worksheet2Part2Q6/Program.cs
@@ -0,0 +1,103 @@
+﻿namespace Worksheet2Part2Q6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputString = "";
+
+
+            Console.WriteLine(GetCardValueAndSuit("QS"));
+            Console.WriteLine(GetCardValueAndSuit("10H"));
+            Console.WriteLine(GetCardValueAndSuit("9c"));
+            Console.WriteLine(GetCardValueAndSuit("JD"));
+
+
+
+
+        }
+
+        static string GetCardValueAndSuit(string abbreviationOfCard)
+        {
+            char suitCharacter = abbreviationOfCard.ToUpper()[abbreviationOfCard.Length - 1];
+
+            return $"{GetCardValue(abbreviationOfCard)} of {GetSuitName(suitCharacter)}";
+        }
+        static string GetCardValue(string inputString)
+        {
+            if (inputString.Length == 3)
+            {
+                return "Ten";
+            }
+
+            string value = "";
+
+            switch (inputString[0])
+            {
+                case 'A':
+                    value = "Ace";
+                    break;
+                case '2':
+                    value = "Two";
+                    break;
+                case '3':
+                    value = "Three";
+                    break;
+                case '4':
+                    value = "Four";
+                    break;
+                case '5':
+                    value = "Five";
+                    break;
+                case '6':
+                    value = "Six";
+                    break;
+                case '7':
+                    value = "Seven";
+                    break;
+                case '8':
+                    value = "Eight";
+                    break;
+                case '9':
+                    value = "Nine";
+                    break;
+                case 'J':
+                    value = "Jack";
+                    break;
+                case 'Q':
+                    value = "Queen";
+                    break;
+                case 'K':
+                    value = "King";
+                    break;
+            }
+
+            return value;
+        }
+
+
+        static string GetSuitName(char suitCharacter)
+        {
+
+            string suit = "";
+
+            switch (suitCharacter)
+            {
+                case 'H':
+                    suit = "Hearts";
+                    break;
+                case 'D':
+                    suit = "Diamonds";
+                    break;
+                case 'C':
+                    suit = "Clubs";
+                    break;
+                case 'S':
+                    suit = "Spades";
+                    break;
+            }
+
+            return suit;
+        }
+    }
+}
